0f054580dee080b285a13b140b08809a1a452380
AguchoD <agu2darelli@gmail.com>
2026-05-01 18:52:48 -0300

    Creación de Metodo 'ObtenerEtiquetaProducto' y Clase 'ProductHelper'

---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal partial class ProductHelper
+    {
+        public string ObtenerEtiquetaProducto (long code, string description, decimal price) => $"[{code}] {description} - {ConversorMoneda(price)}";
+    }
+}
